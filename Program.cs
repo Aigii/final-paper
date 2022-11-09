68b269d351af7baac8cb41ee48e09a160f8c6a0b
[Aigerim] <[t.aigerim19@mail.ru]>
2022-11-09 18:44:33 +0100

    новый массив

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] A;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            A = stroka.Split(' ');
+            var result = new string[A .Length];
+            var realSize = 0;
